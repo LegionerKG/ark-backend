83cfb5c0ed8bd3228a7c917522718f6b0368a952
LegionerKG <mukambetov11@gmail.com>
2025-03-03 21:00:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ark-frontend.onrender.com"],  # Разрешаем фронтенд-домен
+    allow_origins=["http://localhost:3000","https://ark-frontend.onrender.com"],  # Разрешаем фронтенд-домен
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
